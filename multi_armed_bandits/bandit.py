d19a58e6a3ec66e8a78beaa2720ca3a5b4df5a63
hoverslam <hofmann.christopher@outlook.com>
2022-03-09 20:41:56 +0100

    Change variance from random to 1

--- multi_armed_bandits/bandit.py
+++ multi_armed_bandits/bandit.py
@@ -2,18 +2,17 @@ import numpy as np
 
 class Bandit:
     ''' A single slot machine that gives a normally distributed reward 
-    with random mean and variance. '''
+    with random mean and unit variance. '''
     
     def __init__(self):
         self.mean = 5 * np.random.rand()    # from 0 to 5
-        self.variance = np.random.rand()    # from 0 to 1
+        self.variance = 1
         
     def get_reward(self):
         return self.variance * np.random.randn() + self.mean
 
 class MultiArmedBandit:
-    ''' A k-armed slot machine where every lever gives a normally distributed reward
-    with random means and variances. '''    
+    ''' A k-armed slot machine where every lever gives a different reward. '''    
     
     def __init__(self, k):
         self.k = k
@@ -21,7 +20,7 @@ class MultiArmedBandit:
     
     def show_bandits(self):
         for i, b in enumerate(self.bandits):
-            print("[{}] Mean: {:.2f}, Variance: {:.4f}".format(i, b.mean, b.variance))
+            print("[{}] Mean: {:.4f}, Variance: {:.4f}".format(i, b.mean, b.variance))
 
     def get_reward(self, action):
         return self.bandits[action].get_reward()
