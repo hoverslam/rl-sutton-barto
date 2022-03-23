fcd8dd4d3436d7a006da5353eea709ac540b7407
hoverslam <hofmann.christopher@outlook.com>
2022-03-23 22:34:19 +0100

    Add gridworlds

---
+++ dynamic_programming/gridworlds.py
@@ -0,0 +1,111 @@
+# Transition function P for some gridworld MDPs
+# The outer dictionary keys are the states and the inner ones are the actions with a list of all 
+# possible transitions stored in tuples (probability of transition, next state, reward, terminal flag).
+
+class Gridworld_4x4:
+    """ Gridworld 4x4:
+    States: 14 non-terminal states, 2 terminal
+    Actions: up (0), right (1), down (2), left (3) 
+    Reward: -1 for every move
+    Actions that would take the agent off the grid leave its location unchanged.
+    """
+    
+    def __init__(self):
+        self.P = {
+            0: {
+                0: [(1.0, 0, 0.0, True)],
+                1: [(1.0, 0, 0.0, True)],
+                2: [(1.0, 0, 0.0, True)],
+                3: [(1.0, 0, 0.0, True)]
+            },
+            1: {
+                0: [(1.0, 1, -1.0, False)],
+                1: [(1.0, 2, -1.0, False)],
+                2: [(1.0, 5, -1.0, False)],
+                3: [(1.0, 0, -1.0, True)]
+            },
+            2: {
+                0: [(1.0, 2, -1.0, False)],
+                1: [(1.0, 3, -1.0, False)],
+                2: [(1.0, 6, -1.0, False)],
+                3: [(1.0, 1, -1.0, False)]
+            },
+            3: {
+                0: [(1.0, 3, -1.0, False)],
+                1: [(1.0, 3, -1.0, False)],
+                2: [(1.0, 7, -1.0, False)],
+                3: [(1.0, 2, -1.0, False)]
+            },
+            4: {
+                0: [(1.0, 0, -1.0, True)],
+                1: [(1.0, 5, -1.0, False)],
+                2: [(1.0, 8, -1.0, False)],
+                3: [(1.0, 4, -1.0, False)]
+            },
+            5: {
+                0: [(1.0, 1, -1.0, False)],
+                1: [(1.0, 6, -1.0, False)],
+                2: [(1.0, 9, -1.0, False)],
+                3: [(1.0, 4, -1.0, False)]
+            },
+            6: {
+                0: [(1.0, 2, -1.0, False)],
+                1: [(1.0, 7, -1.0, False)],
+                2: [(1.0, 10, -1.0, False)],
+                3: [(1.0, 5, -1.0, False)]
+            },
+            7: {
+                0: [(1.0, 3, -1.0, False)],
+                1: [(1.0, 7, -1.0, False)],
+                2: [(1.0, 11, -1.0, False)],
+                3: [(1.0, 6, -1.0, False)]
+            },
+            8: {
+                0: [(1.0, 4, -1.0, False)],
+                1: [(1.0, 9, -1.0, False)],
+                2: [(1.0, 12, -1.0, False)],
+                3: [(1.0, 8, -1.0, False)]
+            },
+            9: {
+                0: [(1.0, 5, -1.0, False)],
+                1: [(1.0, 10, -1.0, False)],
+                2: [(1.0, 13, -1.0, False)],
+                3: [(1.0, 8, -1.0, False)]
+            },
+            10: {
+                0: [(1.0, 6, -1.0, False)],
+                1: [(1.0, 11, -1.0, False)],
+                2: [(1.0, 14, -1.0, False)],
+                3: [(1.0, 9, -1.0, False)]
+            },
+            11: {
+                0: [(1.0, 7, -1.0, False)],
+                1: [(1.0, 11, -1.0, False)],
+                2: [(1.0, 15, -1.0, True)],
+                3: [(1.0, 10, -1.0, False)]
+            },
+            12: {
+                0: [(1.0, 8, -1.0, False)],
+                1: [(1.0, 13, -1.0, False)],
+                2: [(1.0, 12, -1.0, False)],
+                3: [(1.0, 12, -1.0, False)]
+            },
+            13: {
+                0: [(1.0, 9, -1.0, False)],
+                1: [(1.0, 14, -1.0, False)],
+                2: [(1.0, 13, -1.0, False)],
+                3: [(1.0, 12, -1.0, False)]
+            },
+            14: {
+                0: [(1.0, 10, -1.0, False)],
+                1: [(1.0, 15, -1.0, True)],
+                2: [(1.0, 14, -1.0, False)],
+                3: [(1.0, 13, -1.0, False)]
+            },
+            15: {
+                0: [(1.0, 15, 0.0, True)],
+                1: [(1.0, 15, 0.0, True)],
+                2: [(1.0, 15, 0.0, True)],
+                3: [(1.0, 15, 0.0, True)]
+            },            
+        }        
