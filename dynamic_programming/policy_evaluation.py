1a09f906d5907f5c7c8a2e0b5209e8f3ad21c24b
Christopher Hofmann <hofmann.christopher@outlook.com>
2022-03-25 11:14:07 +0100

    Change the way policies are implemented

--- dynamic_programming/policy_evaluation.py
+++ dynamic_programming/policy_evaluation.py
@@ -1,24 +1,24 @@
-# Evaluate a stochastic policy
+# Evaluate a given policy
 
 import numpy as np
 import gridworlds
 
-def policy_evaluation(pi, P, gamma=1, theta=10e-5):
-    prev_V = np.zeros(len(P))
+def policy_evaluation(pi, env, gamma=1, theta=10e-5):
+    prev_V = np.zeros(len(env.state_space()), dtype=np.float64)
     while True:
-        V = np.zeros(len(P))
-        for s in range(len(P)):            
-            for prob_a, a in pi(s):
-                for prob_s, next_state, reward, done in P[s][a]:
-                    V[s] += prob_a * prob_s * (reward + gamma * prev_V[next_state] * (not done))    
-        
+        V = np.zeros(len(env.state_space()), dtype=np.float64)
+        for s in range(len(env.state_space())):         
+            for a, prob_a in enumerate(pi[s]):
+                for prob_s, next_state, reward, done in env.step(s, a):
+                    V[s] += prob_a * prob_s * (reward + gamma * prev_V[next_state] * (not done))                    
         if np.max(np.abs(prev_V - V)) < theta:
-            break      
-        prev_V = V.copy()
+            break
+        else:      
+            prev_V = V.copy()
+        
+    return V        
 
-    return V           
-
-grid = gridworlds.Grid_4x4()
-pi = lambda s : [(0.25, 0), (0.25, 1), (0.25, 2), (0.25, 3)]    # equiprobable random policy
-V_pi = policy_evaluation(pi, grid.P)
-grid.print_values(V_pi)
+env = gridworlds.Grid_4x4()
+pi = np.ones([len(env.state_space()), len(env.action_space())]) / len(env.action_space())   # equiprobable random policy
+V = policy_evaluation(pi, env)
+env.print_values(V)
