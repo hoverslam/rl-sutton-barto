207556e31ee744d4c9f0bd1a7e5c8198355a7c28
hoverslam <hofmann.christopher@outlook.com>
2022-04-19 17:06:05 +0200

    Remove MC predicition, Add MC control

--- 5_monte_carlo/mc.py
+++ 5_monte_carlo/mc.py
@@ -1,11 +1,23 @@
 import numpy as np
 from tqdm import tqdm
 
-def generate_trajectory(pi, env):
+
+# Monte Carlo Prediction: Sutton & Barto (2018), page 92
+    # [TODO]
+
+
+# GLIE Monte Carlo Control: Introduction to RL with David Silver (2015), Lecture 5
+def epsilon_greedy(Q, state, epsilon):
+    if np.random.rand() < epsilon:
+        return np.random.randint(Q.shape[1])
+    else:
+        return np.argmax(Q[state])
+    
+def generate_trajectory(env, Q, epsilon):
     trajectoy = []
     state, reward, done = env.reset()
     while True:
-        action = pi(state)
+        action = epsilon_greedy(Q, state, epsilon)
         next_state, reward, done = env.step(action)
         if not done:
             trajectoy.append((state, action, next_state, reward, done))
@@ -13,28 +25,24 @@ def generate_trajectory(pi, env):
         else:
             break
         
-    return np.array(trajectoy, dtype=object)     
+    return np.array(trajectoy, dtype=object)  
 
-# Monte Carlo Prediction: Sutton & Barto (2018), page 92
-def mc_prediction(pi, env, gamma=1.0, episodes=500, first_visit=True):
+def mc_control(env, gamma=1.0, episodes=500):
     nS = len(env.state_space())
-    V = np.zeros(nS, dtype=np.float64)
-    returns = np.zeros(nS, dtype=np.float64)
-    counts = np.zeros(nS, dtype=np.float64)
+    nA = len(env.action_space())
+    Q = np.zeros((nS, nA), dtype=np.float64)
+    counts = np.zeros((nS, nA), dtype=np.int32)
+    discounts = np.logspace(0, env.max_steps, num=env.max_steps, base=gamma, endpoint=False)
     
-    for e in tqdm(range(episodes)):
-        trajectory = generate_trajectory(pi, env)
-        visited = np.zeros(nS, dtype=bool)
-        G = 0        
-        for state, _, _, reward, _ in trajectory:
-            G = gamma * G + reward
-            if visited[state] and first_visit:
-                continue            
-            visited[state] = True
-            returns[state] += G
-            counts[state] += 1
-    
-    for i in range(len(V)):
-        V[i] = returns[i] / counts[i]
-    
-    return V
+    for e in tqdm(range(episodes)):      
+        epsilon = 1 / (e + 1)
+        trajectory = generate_trajectory(env, Q, epsilon)
+        for t, (state, action, _, reward, _) in enumerate(trajectory):            
+            counts[state][action] += 1
+            steps = len(trajectory[t:])
+            G = np.sum(discounts[:steps] * trajectory[t:, 3])
+            Q[state][action] = Q[state][action] + (1 / counts[state][action]) * (G - Q[state][action])
+            
+    pi = np.argmax(Q, axis=1)
+
+    return (pi, Q)
