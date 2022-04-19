06e0c8fd26a07fdb5dab51b65ae793dd124a16d7
hoverslam <hofmann.christopher@outlook.com>
2022-04-19 18:37:37 +0200

    Add SARSA without GLIE

--- 6_temporal_difference/td_learning.py
+++ 6_temporal_difference/td_learning.py
@@ -10,14 +10,40 @@ def epsilon_greedy(Q, state, epsilon):
         return np.argmax(Q[state])
 
 
-# SARSA with GLIE: Sutton & Barto (2018), p. 130
-def sarsa(env, gamma=1.0, alpha=0.1, episodes=10000):
+# SARSA: Sutton & Barto (2018), p. 130
+def sarsa(env, gamma=1.0, epsilon= 0.1, alpha=0.1, episodes=10000):
     nS = len(env.state_space())
     nA = len(env.action_space())
     Q = np.zeros((nS, nA), dtype=np.float64)
     
     for e in tqdm(range(episodes)):
-        epsilon = 1 / (e + 1)
+        state, reward, done = env.reset()
+        action = epsilon_greedy(Q, state, epsilon)        
+                
+        while not done:
+            next_state, reward, done = env.step(action)
+            next_action = epsilon_greedy(Q, state, epsilon)
+            
+            # Update Q values using the action taken in the next state
+            td_target = reward + gamma * Q[next_state][next_action] * (not done)
+            td_error = td_target - Q[state][action]
+            Q[state][action] += alpha * td_error
+
+            state, action = next_state, next_action
+            
+    pi = np.argmax(Q, axis=1)
+            
+    return (pi, Q)
+
+
+# SARSA with GLIE: Introduction to RL with David Silver (2015), Lecture 5
+def sarsa_glie(env, gamma=1.0, alpha=0.1, episodes=10000):
+    nS = len(env.state_space())
+    nA = len(env.action_space())
+    Q = np.zeros((nS, nA), dtype=np.float64)
+    
+    for e in tqdm(range(episodes)):
+        epsilon = 1 / (e + 1)    # GLIE -> Greedy in the Limit with Infinite Exploration
         state, reward, done = env.reset()
         action = epsilon_greedy(Q, state, epsilon)        
                 
